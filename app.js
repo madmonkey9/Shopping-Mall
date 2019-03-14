2233ce9e1ac07c0cd670818a00ec13283a9a5227
madmonkey <madmonkey3822@gmail.com>
2019-03-14 22:45:22 +0900

    add ejs

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require("express");
-const router = require("./routes/admin");
 const path = require("path");
+
+const router = require("./routes/admin");
 
 const app = express();
 const port = 3000;
+
+// 확장자가 ejs 로 끝나는 뷰 엔진을 추가.
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+console.log(__dirname);
 
 app.get("/", function(_, res) {
   res.send("first app");
--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,11 @@ const router = Router();
 
 router.get("/", (_, res) => res.send("admin URL"));
 
-router.get("/products", (_, res) => res.send("products URL"));
-
+router.get("/products", (_, res) => {
+  // res.send("products URL")
+  res.render("admin/products", {
+    message: "hello",
+    school: "nodejs"
+  });
+});
 module.exports = router;
